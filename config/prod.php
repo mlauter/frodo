8ca6d58bfaa6be95d50712367dab6f74aa44d035
Miriam Lauter <lauter.miriam@gmail.com>
2019-04-14 15:33:18 -0400

    Add prod config

--- config/prod.php
+++ config/prod.php
@@ -1,8 +1,16 @@
 <?php declare(strict_types=1);
 
 /**
- * Production server config
+ * Development server config
  */
-$config = [];
+$config = [
+    'logger' => [
+        'path' => '/var/log/apache2/frodo.log',
+        'level' => Monolog\Logger::WARNING,
+    ],
+    'db' => [
+        'file' => dirname(__DIR__) . '/data/frodo.db',
+    ]
+];
 
 $GLOBALS['server_config'] = $config;
